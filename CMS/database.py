309258202a85e510b7df593480ac0e70c9a0e584
Vaishali32424 <vaishaliparmar7722@example.com>
2025-11-11 17:21:10 +0530

    added Procfile

--- CMS/database.py
+++ CMS/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+import urllib.parse
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}" #By changing the URL we can connect any DataBase platform like PostgrSql etc.
 
 # DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/GreenProducts"
 
-DATABASE_URL = "mysql+pymysql://root:YourPassword@localhost:3306/GreenProducts"
+# DATABASE_URL = "mysql+pymysql://root:YourPassword@localhost:3306/GreenProducts"
 
 engine = create_engine(DATABASE_URL)
 
